ff335bbc4983ba8e245c805c31cd6d6ae6f6d334
Catalina Brochero <catabrochero18@gmail.com>
2025-03-24 22:47:04 -0300

    ajuste PEP8

--- questions.py
+++ questions.py
@@ -1,26 +1,26 @@
 import random
 # Preguntas para el juego
-questions = [
-"¿Qué función se usa para obtener la longitud de una cadena en Python?",
-"¿Cuál de las siguientes opciones es un número entero en Python?",
-"¿Cómo se solicita entrada del usuario en Python?",
-"¿Cuál de las siguientes expresiones es un comentario válido en Python?",
-"¿Cuál es el operador de comparación para verificar si dos valores son iguales?",
-]
+questions = ["¿Qué función se usa para obtener la longitud de una cadena en "
+             "Python?",
+             "¿Cuál de las siguientes opciones es un número entero en "
+             "Python?",
+             "¿Cómo se solicita entrada del usuario en Python?",
+             "¿Cuál de las siguientes expresiones es un comentario válido "
+             "en Python?",
+             "¿Cuál es el operador de comparación para verificar si dos "
+             "valores son iguales?",
+             ]
 # Respuestas posibles para cada pregunta, en el mismo orden
 # que las preguntas
-answers = [
-("size()", "len()", "length()", "count()"),
-("3.14", "'42'", "10", "True"),
-("input()", "scan()", "read()", "ask()"),
-(
-"// Esto es un comentario",
-"/* Esto es un comentario */",
-"-- Esto es un comentario",
-"# Esto es un comentario",
-),
-("=", "==", "!=", "==="),
-]
+answers = [("size()", "len()", "length()", "count()"),
+           ("3.14", "'42'", "10", "True"),
+           ("input()", "scan()", "read()", "ask()"),
+           ("// Esto es un comentario",
+            "/* Esto es un comentario */",
+            "-- Esto es un comentario",
+            "# Esto es un comentario"),
+           ("=", "==", "!=", "==="),
+           ]
 # Índice de la respuesta correcta para cada pregunta, el
 # mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
@@ -28,25 +28,26 @@ correct_answers_index = [1, 2, 0, 3, 1]
 
 score = 0
 
-#creo una lista de tuplas con 3 preguntas aleatorias, sus opciones y rtas (sin repetir preguntas)
-questions_to_ask = random.sample(list(zip(questions, 
-answers, correct_answers_index)), k=3)
+# creo una lista de tuplas con 3 preguntas aleatorias, sus opciones y rtas
+# (sin repetir preguntas)
+questions_to_ask = random.sample(list(zip(questions, answers,
+                                          correct_answers_index)), k=3)
 for qs, ans, cor_ans in questions_to_ask:
     # Se muestra la pregunta y las respuestas posibles
     print(qs)
     for i, answer in enumerate(ans):
         print(f"{i + 1}. {answer}")
-     # El usuario tiene 2 intentos para responder
-     # correctamente
+    # El usuario tiene 2 intentos para responder
+    # correctamente
     for intento in range(2):
         user_answer = input("Respuesta: ")
-        if(not user_answer.isdigit() or not (1 <= int(user_answer) <= 4)): 
+        if not user_answer.isdigit() or not (1 <= int(user_answer) <= 4):
             print("respuesta no válida")
             exit(1)
         user_answer = int(user_answer) - 1
         # Se verifica si la respuesta es correcta
         if user_answer == cor_ans:
-            score+=1
+            score += 1
             print("¡Correcto!")
             break
         score -= 0.5
