f7c403223212efd742ba33aca36ad1cdaa16befa
Catalina Brochero <catabrochero18@gmail.com>
2025-03-24 22:37:11 -0300

    ajuste PEP8

--- actividad_extra.py
+++ actividad_extra.py
@@ -13,16 +13,16 @@ Mostrar el inventario actual, que incluya el nombre y la cantidad de cada
 producto.
 Salir del programa.
 El inventario puede ser almacenado utilizando un diccionario simple, donde
-las claves sean los nombres de los productos y los valores sean las cantidades. Se
-deben manejar situaciones simples como la introducción de productos duplicados
-o la eliminación de productos inexistentes.
+las claves sean los nombres de los productos y los valores sean las cantidades.
+Se deben manejar situaciones simples como la introducción de productos
+duplicados o la eliminación de productos inexistentes.
 """
-import random 
+import random
 products = {}
 seguir = True
 nom_inv = "[untitled]"
 options = [
-    "Mostrar el inventario", 
+    "Mostrar el inventario",
     "Agregar nuevo producto",
     "Eliminar producto",
     "Consultar stock de un producto",
@@ -31,51 +31,58 @@ options = [
     "Cerrar inventario"
 
 ]
-while(seguir):
+while (seguir):
     print(f"Inventario {nom_inv}")
     print(f"Seleccione una opción")
     for i, op in enumerate(options):
-        print(f"{i+1}. {op}") 
+        print(f"{i+1}. {op}")
     print()
     op_chosen = input()
     print()
-    if op_chosen.isnumeric(): op_chosen = int(op_chosen)
+    if op_chosen.isnumeric():
+        op_chosen = int(op_chosen)
     match op_chosen:
         case 1:
             if len(products) == 0:
-                print("Inventario vacío. ") 
+                print("Inventario vacío.")
             else:
                 for nom, stock in products.items():
-                    print(f"Nombre del producto: {nom}. \nStock disponible: {stock} \n")
+                    print(f"Nombre del producto: {nom}. \n"
+                          f"Stock disponible: {stock} \n")
         case 2:
-            print("Ingrese el nombre del producto y la cantidad de unidades disponibles")
+            print("Ingrese el nombre del producto y "
+                  "la cantidad de unidades disponibles")
             new_name = input("Nombre del producto: ")
             new_stock = input("unidades disponibles: ")
             if new_name in products:
-                user_choice = input(f"Ya existe un producto con ese nombre. Desea sobreescribirlo? \n"
-                    f"1. Sí \n"
-                    f"2. No \n"
-                    )
-                if not user_choice.isnumeric() or not (1 <= int(user_choice) <= 2):
+                user_choice = input(f"Ya existe un producto con ese nombre. "
+                                    f"Desea sobreescribirlo? \n"
+                                    f"1. Sí \n"
+                                    f"2. No \n"
+                                    )
+                if not user_choice.isnumeric() or not (1 <=
+                                                       int(user_choice) <= 2):
                     print("Opción inválida. Inventario no actualizado.")
                 else:
                     user_choice = int(user_choice)
                     if user_choice == 1:
-                        if not new_stock.isnumeric(): 
-                            print("Cantidad inválida. Inventario no actualizado.")
+                        if not new_stock.isnumeric():
+                            print("Cantidad inválida."
+                                  "Inventario no actualizado.")
                         else:
                             products[new_name] = int(new_stock)
                             print("Inventario actualizado.")
                     else:
                         print("Inventario no actualizado.")
             else:
-                if not new_stock.isnumeric(): 
+                if not new_stock.isnumeric():
                     print("Cantidad inválida. Inventario no actualizado.")
                 else:
                     products[new_name] = int(new_stock)
                     print("Inventario actualizado.")
         case 3:
-            user_input = input(f"Ingrese el nombre del producto que quiere eliminar (cc para cancelar): ")
+            user_input = input(f"Ingrese el nombre del producto que quiere "
+                               f"eliminar (cc para cancelar): ")
             if user_input in products:
                 products.pop(user_input)
                 print("Inventario actualizado.")
@@ -92,11 +99,14 @@ while(seguir):
         case 5:
             user_input = input(f"Ingrese el nombre del producto: ")
             if user_input in products:
-                user_choice = input(f"1.Agregar unidades \n2.Descontar unidades \n")
-                if user_choice.isnumeric(): user_choice = int(user_choice)
+                user_choice = input(f"1.Agregar unidades \n"
+                                    f"2.Descontar unidades \n")
+                if user_choice.isnumeric():
+                    user_choice = int(user_choice)
                 match user_choice:
                     case 1:
-                        un = input(f"Ingrese la cantidad de unidades que quiere agregar: ")
+                        un = input(f"Ingrese la cantidad de unidades que "
+                                   f"quiere agregar: ")
                         if un.isnumeric():
                             un = int(un)
                             products[user_input] = products[user_input] + un
@@ -104,20 +114,24 @@ while(seguir):
                         else:
                             print("Valor inválido. Inventario no actualizado.")
                     case 2:
-                        un = input(f"Ingrese la cantidad de unidades que quiere descontar: ")
+                        un = input(f"Ingrese la cantidad de unidades que "
+                                   f"quiere descontar: ")
                         if un.isnumeric():
                             un = int(un)
-                            products[user_input] = max(0, products[user_input] - un)
+                            products[user_input] = max(0,
+                                                       products[user_input]
+                                                       - un)
                             print("Inventario actualizado.")
                         else:
-                            print("Valor inválido. Inventario no actualizado.")                    
+                            print("Valor inválido. Inventario no actualizado.")
                     case _:
                         print("Opción inválida. Inventario no actualizado.")
             else:
                 print("Producto no encontrado.")
         case 6:
-            user_input = input(f"Ingrese el nuevo nombre del inventario (cc para cancelar): ")
-            if(user_input == "cc"):
+            user_input = input(f"Ingrese el nuevo nombre del inventario "
+                               f"(cc para cancelar): ")
+            if user_input == "cc":
                 print("Operación cancelada.")
             else:
                 nom_inv = user_input
@@ -130,11 +144,13 @@ while(seguir):
     ok = False
     while not ok:
         user_choice = input(f"1.Ver más opciones \n2.Cerrar inventario\n\n")
-        if not user_choice.isnumeric() or not(1 <= int(user_choice) <= 2): #seria mejor hacerlo con un match como arriba?
+        # seria mejor hacerlo con un match como arriba?
+        if not user_choice.isnumeric() or not (1 <= int(user_choice) <= 2):
             print(f"Ingrese una opción válida (1 o 2).")
-        else: #opcion valida 
+        else:  # opcion valida
             user_choice = int(user_choice)
             ok = True
-            if(user_choice == 2): seguir = False #tmb podria usar un while true y hacer un exit acá
+            if user_choice == 2:
+                seguir = False  # podria usar while(true) y hacer un exit(0)
         print()
 print("Inventario cerrado.")
